435eeb6e08cf0b2b14c9177812f81cdaef928964
Anne Trent <anne.trent1@gmail.com>
2017-08-30 16:30:48 -0600

    add tile

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Content from '../ui/content.js'
 import GlobalStyles from '../ui/global-styles.js'
 import Head from '../ui/head.js'
 import Header from '../ui/header.js'
+import Tile from '../ui/tile.js'
 
 export default () => 
   <div>
@@ -10,5 +11,8 @@ export default () =>
     <Content>
       <Header />
       <hr />
+      <Tile heading="Heading">
+        Sweet roll gingerbread jelly beans wafer jelly bonbon sweet macaroon halvah. Candy sweet apple pie apple pie cookie. Lemon drops fruitcake jelly-o apple pie candy jelly-o candy danish cookie. Brownie soufflé pie sugar plum pastry. Jelly gummi bears wafer sugar plum brownie apple pie cotton candy. Chocolate bear claw apple pie. Muffin tart liquorice pie donut soufflé. Cookie brownie pie cotton candy cake carrot cake tiramisu sesame snaps. Tart cheesecake chupa chups.
+      </Tile>
     </Content>
   </div>
---
+++ ui/tile.js
@@ -0,0 +1,8 @@
+export default props =>
+<div className="tile">
+  <h2>{props.heading}</h2>
+  <p>{props.children}</p>
+  <style jsx>{`
+    
+  `}</style>
+</div>
