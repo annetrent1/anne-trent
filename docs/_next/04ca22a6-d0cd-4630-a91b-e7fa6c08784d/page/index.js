c9be58ceedca988ca26142a1cdaaefab455f5e15
Anne Trent <anne.trent1@gmail.com>
2017-08-30 19:56:03 -0600

    chore: regen site

--- docs/_next/04ca22a6-d0cd-4630-a91b-e7fa6c08784d/page/index.js
+++ docs/_next/04ca22a6-d0cd-4630-a91b-e7fa6c08784d/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([2],[],[362]);
+            var comp = module.exports=webpackJsonp([2],[],[364]);
             return { page: comp.default }
           })
         
