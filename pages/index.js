807044f01fac5cd66fe475b68678db1210adeb14
Anne Trent <anne.trent1@gmail.com>
2017-08-30 19:55:06 -0600

    cname correct

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,12 @@ export default () =>
       <Header />
       <hr />
       <Tile heading="Heading">
-        Sweet roll gingerbread jelly beans wafer jelly bonbon sweet macaroon halvah. Candy sweet apple pie apple pie cookie. Lemon drops fruitcake jelly-o apple pie candy jelly-o candy danish cookie. Brownie soufflé pie sugar plum pastry. Jelly gummi bears wafer sugar plum brownie apple pie cotton candy. Chocolate bear claw apple pie. Muffin tart liquorice pie donut soufflé. Cookie brownie pie cotton candy cake carrot cake tiramisu sesame snaps. Tart cheesecake chupa chups.
+        Sweet roll gingerbread jelly beans wafer jelly bonbon sweet macaroon halvah. 
+        Candy sweet apple pie apple pie cookie. Lemon drops fruitcake jelly-o apple pie candy
+        jelly-o candy danish cookie. Brownie soufflé pie sugar plum pastry. Jelly gummi bears 
+        wafer sugar plum brownie apple pie cotton candy. Chocolate bear claw apple pie. Muffin 
+        tart liquorice pie donut soufflé. Cookie brownie pie cotton candy cake carrot cake 
+        ßtiramisu sesame snaps. Tart cheesecake chupa chups.
       </Tile>
     </Content>
   </div>
